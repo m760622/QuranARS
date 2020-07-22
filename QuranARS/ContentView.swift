dc644acdb7ab75927bf4faa581f7409c7e322549
Mohammed Abunada <m7606225@gmail.com>
2020-07-22 03:18:34 +0200

    Initial Commit

---
+++ QuranARS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  QuranARS
+//
+//  Created by Mohammed Abunada on 2020-07-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuranARS/QuranARSApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranARSApp.swift
+//  QuranARS
+//
+//  Created by Mohammed Abunada on 2020-07-22.
+//
+
+import SwiftUI
+
+@main
+struct QuranARSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
